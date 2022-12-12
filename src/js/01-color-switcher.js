9f87b0f48fa75f0b39c4e124e48e7527a2e7e811
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2022-12-12 15:38:44 +0200

    add timer

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,42 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+
+let idInterval = null;
+
+refs.btnStart.addEventListener('click', onColorChangeStart);
+refs.btnStop.addEventListener('click', onColorChangeStop);
+
+// refs.btnStop.setAttribute('disabled', '');
+
+function onColorChangeStart(evt) {
+  onBtnDisabled();
+  // evt.target.setAttribute('disabled', '');
+  // refs.btnStop.removeAttribute('disabled');
+  idInterval = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onColorChangeStop(evt) {
+  // evt.target.setAttribute('disabled', '');
+  // refs.btnStart.removeAttribute('disabled');
+  onBtnDisabled();
+  clearInterval(idInterval);
+}
+
+function onBtnDisabled() {
+  if (!refs.btnStart.disabled) {
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+  } else {
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+  }
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,85 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+
+// one by one
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+Notify.init({
+  position: 'center-top',
+  timeout: 10000,
+  clickToClose: true,
+  pauseOnHover: true,
+});
+
+const refs = {
+  inputDateEl: document.querySelector('#datetime-picker'),
+  timerEl: document.querySelector('.timer'),
+  startBtn: document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+};
+
+refs.startBtn.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      Notify.failure('Please choose a date in the future');
+      return;
+    } else console.log(selectedDates[0]);
+    refs.startBtn.disabled = false;
+  },
+};
+
+flatpickr(refs.inputDateEl, options);
+
+refs.startBtn.addEventListener('click', onTimerStart);
+
+function onTimerStart() {
+  Notify.success('Countdown started');
+  let insertedDate = new Date(refs.inputDateEl.value);
+  let timer = setInterval(() => {
+    let countdown = insertedDate - new Date();
+    if (countdown > 0) {
+      let timeObject = convertMs(countdown);
+      refs.days.textContent = addLeadingZero(timeObject.days);
+      refs.hours.textContent = addLeadingZero(timeObject.hours);
+      refs.minutes.textContent = addLeadingZero(timeObject.minutes);
+      refs.seconds.textContent = addLeadingZero(timeObject.seconds);
+    } else if (countdown <= 0) {
+      Notify.success('Countdown finished');
+      clearInterval(timer);
+    }
+  }, 1000);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart();
+}
